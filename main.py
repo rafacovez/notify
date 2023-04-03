652f592da231ec52c694015a7828b202deb151cb
Adán Estévez <adanescollante@gmail.com>
2023-04-03 16:02:31 -0400

    update bot

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-import os
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
 
-port = int(os.environ.get("PORT", 5000))
+def start(update:Update, context:CallbackContext):
+    update.message.reply_text("Hey buddy!")
+
+if __name__ == "__main__":
+    updater = Updater("6060982771:AAF_WeWw9CYT737td2YZupDF4ivZhKkYp6E")
+    
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+
+    updater.start_polling()
+    updater.idle()
