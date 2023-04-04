ede6bd887b4c05e87bad2dadd07e266c68cd52a5
Adán Estévez <adanescollante@gmail.com>
2023-04-04 13:18:30 -0400

    updated code

--- main.py
+++ main.py
@@ -1,15 +1,66 @@
+from typing import Final
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
-def start(update:Update, context:CallbackContext):
-    update.message.reply_text("Hey buddy!")
+TOKEN: Final = "6060982771:AAF_WeWw9CYT737td2YZupDF4ivZhKkYp6E"
+BOT_USERNAME: Final = "@playlistNotificationBot"
+
+# Commands
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello, thanks for chatting with me.")
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("I'll send you a notification when a new song is added to your collaborative playlists.")
+
+async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("This is a custom command.")
+
+# Responses
+def handle_response(text: str) -> str:
+    message: str = text.lower()
+
+    if "hello" in message:
+        return "Hey there!"
+    
+    if "how are you" in message:
+        return "I'm great!"
+    
+    return "I don't get it"
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message_type: str = update.message.chat.type
+    text: str = update.message.text
+
+    print(f'User ({update.message.chat.id}) in {message_type}: "{text}"')
+
+    if message_type === "group":
+        if BOT_USERNAME in text:
+            new_text: str = text.replace(BOT_USERNAME, "").strip()
+            response: str = handle_response(new_text)
+        else:
+            return
+    else:
+        response: str = handle_response(text)
+    
+    print("Bot:", response)
+    await update.message.reply_text(response)
+
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f'Update {update} caused error {context.error}')
 
 if __name__ == "__main__":
-    updater = Updater("6060982771:AAF_WeWw9CYT737td2YZupDF4ivZhKkYp6E")
-    
-    dp = updater.dispatcher
+    print("Starting bot...")
+    app = Application.builder().token(TOKEN).build()
 
-    dp.add_handler(CommandHandler("start", start))
+    # Commands
+    app.add_handler(CommandHandler("start", start_command))
+    app.add_handler(CommandHandler("help", help_command))
+    app.add_handler(CommandHandler("custom", custom_command))
 
-    updater.start_polling()
-    updater.idle()
+    # Messages
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+
+    # Errors
+    app.add_error_handler(error)
+
+    app.run_polling(poll_interval=3)
