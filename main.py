c5d0822cc58d1c79deda37ea40a7dc6544be0d3a
Adán Estévez <adanescollante@gmail.com>
2023-04-07 17:20:18 -0400

    test code

--- main.py
+++ main.py
@@ -1,66 +1,21 @@
-from typing import Final
-from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+import telegram
+from telegram.ext import Updater, CommandHandler
 
-TOKEN: Final = "6060982771:AAF_WeWw9CYT737td2YZupDF4ivZhKkYp6E"
-BOT_USERNAME: Final = "@playlistNotificationBot"
+# Replace YOUR_BOT_TOKEN with your actual bot token
+bot_token = "6060982771:AAF_WeWw9CYT737td2YZupDF4ivZhKkYp6E"
+bot_username = "@playlistNotificationBot"
 
-# Commands
-async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello, thanks for chatting with me.")
+# Define the /start command handler
+def start(update, context):
+    # Send a message to the user who typed /start
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hi!")
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("I'll send you a notification when a new song is added to your collaborative playlists.")
+# Set up the Telegram bot
+updater = Updater(token=bot_token, use_context=True)
+dispatcher = updater.dispatcher
 
-async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("This is a custom command.")
+# Register the /start command handler
+dispatcher.add_handler(CommandHandler("start", start))
 
-# Responses
-def handle_response(text: str) -> str:
-    message: str = text.lower()
-
-    if "hello" in message:
-        return "Hey there!"
-    
-    if "how are you" in message:
-        return "I'm great!"
-    
-    return "I don't get it"
-
-async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    message_type: str = update.message.chat.type
-    text: str = update.message.text
-
-    print(f'User ({update.message.chat.id}) in {message_type}: "{text}"')
-
-    if message_type == "group":
-        if BOT_USERNAME in text:
-            new_text: str = text.replace(BOT_USERNAME, "").strip()
-            response: str = handle_response(new_text)
-        else:
-            return
-    else:
-        response: str = handle_response(text)
-    
-    print("Bot:", response)
-    await update.message.reply_text(response)
-
-async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print(f'Update {update} caused error {context.error}')
-
-if __name__ == "__main__":
-    print("Starting bot...")
-    app = Application.builder().token(TOKEN).build()
-
-    # Commands
-    app.add_handler(CommandHandler("start", start_command))
-    app.add_handler(CommandHandler("help", help_command))
-    app.add_handler(CommandHandler("custom", custom_command))
-
-    # Messages
-    app.add_handler(MessageHandler(filters.TEXT, handle_message))
-
-    # Errors
-    app.add_error_handler(error)
-
-    app.run_polling(poll_interval=3)
+# Start the bot
+updater.start_polling()
