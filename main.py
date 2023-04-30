10d09d1532b22ffc37f2aebab5dfa43571cd8481
Adán Estévez <adanescollante@gmail.com>
2023-04-30 15:04:04 -0400

    coded /help command handler and a catch-all handler dor any input that's not a recognized command

--- main.py
+++ main.py
@@ -10,10 +10,26 @@ TOKEN = os.getenv("BOT_API_TOKEN")
 # Initialize the bot
 bot = telebot.TeleBot(TOKEN)
 
-# Handle the /start command
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "Hi!")
+# Define the help command handler
+@bot.message_handler(commands=['help'])
+def help_command_handler(message):
+    chat_id = message.chat.id
+    bot_reply = "I can help you get notified when your friends add or remove a song from the playlists you share together.\n\t\n\tYou can make me do this for you by using these commands:\n\t\n\tSet up Spotify:\n\t/logintospotify\n\t\n\tAdd a playlist:\n\t/addplaylist\n\t\n\tRemove a playlist:\n\t/removeplaylist"
+    bot.send_message(chat_id, bot_reply)
+
+# Handle any message that's not a recognized command
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def message_handler(message):
+    message_text = message.text
+    chat_id = message.chat.id
+    bot_help_reply = "I can help you get notified when your friends add or remove a song from the playlists you share together.\n\t\n\tYou can make me do this for you by using these commands:\n\t\n\tSet up Spotify:\n\t/logintospotify\n\t\n\tAdd a playlist:\n\t/addplaylist\n\t\n\tRemove a playlist:\n\t/removeplaylist"
+    bot_contribute_reply = "My creator didn't think about that command. <a href='https://github.com/rafacovez/notify'>is it a good idea though?</a>"
+    if message_text.startswith('/'):
+        # Handle unknown commands
+        bot.send_message(chat_id, bot_contribute_reply, parse_mode="HTML")
+    else:
+        # Handle regular text
+        bot.send_message(chat_id, bot_help_reply)
 
 # Start the bot
 bot.polling()
