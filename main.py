8d23d895c00f5b9fd44fe47c5e58edf3230d1737
Adán Estévez <adanescollante@gmail.com>
2023-07-06 22:06:41 -0400

    refactored code in a OOP manner using python classes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth  # spotify authentication handler
 from telebot import TeleBot  # telegram bots interaction library
-from telebot.types import BotCommand, Message
+from telebot.types import *
 
 from my_functions import *
 
@@ -14,9 +14,11 @@ load_dotenv()
 
 
 class Database:
-    def __init__(self, database: str) -> None:
+    def __init__(self, database: str, backup: str = "backup.db") -> None:
         self.database: str = database
+        self.backup: str = backup
         self.conn: sqlite3.Connection = None
+        self.backup_conn: sqlite3.Connection = None
         self.cursor: sqlite3.Cursor = None
 
     def __do_nothing(self) -> None:
@@ -25,7 +27,6 @@ class Database:
     def __connect(self) -> None:
         try:
             self.conn = sqlite3.connect(self.database)
-            print(f"Successfully connected to {self.database}")
 
         except sqlite3.Error as e:
             print(f"Error connecting to database: {e}")
@@ -35,28 +36,48 @@ class Database:
     def __disconnect(self) -> None:
         try:
             self.conn.commit()
-            print(f"Successfully commited changes to {self.database}")
+            self.backup_conn = sqlite3.connect(self.backup)
+            self.conn.backup(self.backup_conn)
+            self.backup_conn.close()
         except sqlite3.Error as e:
-            print(f"Error commiting changes to {self.database}: {e}")
+            print(f"Error commiting changes: {e}")
         self.cursor.close()
         self.conn.close()
 
-    def process(self, func=None) -> None:
+    def process(self, func: Callable = None) -> Any:
         if func is None:
             self.__do_nothing()
         else:
             self.__connect()
-            func()
+            result = func()
             self.__disconnect()
+            return result
 
-    def do_something(self) -> None:
-        def logic():
-            print("It's alive! IT'S ALIVEEE!")
+    def create_users_table(self) -> None:
+        def logic() -> None:
+            self.cursor.execute(
+                "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, telegram_user_id INTEGER, spotify_user_display TEXT, spotify_user_id TEXT, refresh_token TEXT, access_token TEXT)"
+            )
 
         self.process(logic)
 
+    def user_exists(self, user: int) -> bool:
+        def logic() -> bool:
+            self.cursor.execute(
+                "SELECT telegram_user_id FROM users WHERE telegram_user_id = ?",
+                (user,),
+            )
+            user_id: int = self.cursor.fetchone()
 
-class Spotify:
+            if user_id is None:
+                return False
+            else:
+                return True
+
+        return self.process(logic)
+
+
+class SpotifyHandler:
     def __init__(
         self, client_id: str, client_secret: str, redirect_uri: str, scope: str
     ) -> None:
@@ -64,24 +85,43 @@ class Spotify:
         self.client_secret: str = client_secret
         self.redirect_uri: str = redirect_uri
         self.scope: str = scope
+        self.sp_oauth: SpotifyOAuth = SpotifyOAuth(
+            client_id=self.client_id,
+            client_secret=self.client_secret,
+            redirect_uri=self.redirect_uri,
+            scope=self.scope,
+        )
+        self.sp: Spotify = Spotify(oauth_manager=self.sp_oauth)
+        self.user_sp: Optional[Spotify] = None
 
     def __do_nothing(self) -> None:
         pass
 
+    def get_user_sp(self, access_token: str) -> Spotify:
+        self.user_sp: Spotify = Spotify(auth=access_token)
+        return self.user_sp
+
 
 class NotifyBot:
-    def __init__(
-        self,
-        bot_token: str,
-        spotify: Spotify,
-        database: sqlite3.Connection,
-    ) -> None:
+    def __init__(self, bot_token: str, spotify: Spotify, database: Database) -> None:
         self.bot_token: str = bot_token
         self.bot: TeleBot = TeleBot(self.bot_token)
         self.spotify: Spotify = spotify
-        self.database: sqlite3.Connection = database
+        self.database: Database = database
+        self.message: Optional[Message] = None
+        self.user_id: Optional[int] = None
+        self.chat_id: Optional[int] = None
         self.bot.register_message_handler(self.handle_message)
         self.commands: Dict[str, Dict[str, Union[Callable[..., Any], str]]] = {
+            "start": {"func": self.start, "desc": "Starts Notify"},
+            "help": {
+                "func": self.start,
+                "desc": "Provides help for using Notify",
+            },
+            "login": {
+                "func": self.auth_user,
+                "desc": "Authorize Notify to access your Spotify account.",
+            },
             "notify": {
                 "func": self.__do_nothing,
                 "desc": "Start tracking a playlist to get notified when someone else adds or removes a song from it.",
@@ -121,34 +161,53 @@ class NotifyBot:
     def __do_nothing(self) -> None:
         pass
 
+    def auth_user(self) -> None:
+        if self.database.user_exists(self.user_id):
+            self.bot.send_message(self.chat_id, "You're already logged in.")
+        else:
+            auth_url: Any = self.spotify.sp_oauth.get_authorize_url(state=self.user_id)
+            self.bot.send_message(
+                self.chat_id,
+                f"Please <a href='{auth_url}'>authorize me</a> to access your Spotify account, then try again.",
+                parse_mode="HTML",
+            )
+
+    def start(self) -> None:
+        self.bot.send_message(self.chat_id, "Start")
+
     def last_played(self) -> None:
         print("Last played func logic!")
 
     def determine_function(self, message: Message) -> None:
-        if message.content_type == "text":
-            message_text: str = message.text.strip()
-            chat_id: int = message.chat.id
-            command_exists: bool = False
+        self.message = message
+        message_text: str = self.message.text.strip()
+        self.user_id: int = self.message.from_user.id
+        self.chat_id: int = self.message.chat.id
+        command_exists: bool = False
 
-            if message_text.startswith("/"):
-                command: str = message.text
-                for command_item in self.command_list:
-                    if command == command_item.command:
+        if message_text.startswith("/"):
+            command: str = self.message.text
+            for command_item in self.command_list:
+                if command == command_item.command:
+                    command_exists = True
+                    self.database.create_users_table()
+                    if self.database.user_exists(self.user_id):
                         command_func: function = self.commands[
                             command_item.command.strip("/")
                         ]["func"]
                         command_func()
-                        command_exists = True
+                    else:
+                        self.auth_user()
 
-                if not command_exists:
-                    self.bot.send_message(
-                        chat_id,
-                        "My creators didn't think about that one yet! <a href='https://github.com/rafacovez/notify'>is it a good idea though?</a>",
-                        parse_mode="HTML",
-                    )
+            if not command_exists:
+                self.bot.send_message(
+                    self.chat_id,
+                    "My creators didn't think about that one yet! <a href='https://github.com/rafacovez/notify'>is it a good idea though?</a>",
+                    parse_mode="HTML",
+                )
 
-            else:
-                self.bot.send_message(chat_id, "Sorry, I only speak commands...")
+        else:
+            self.bot.send_message(self.chat_id, "Sorry, I only speak commands...")
 
     def handle_message(self, message: Message) -> None:
         if message.content_type == "text":
@@ -158,11 +217,15 @@ class NotifyBot:
         print("Notify started!")
         self.bot.infinity_polling()
 
+    def stop_listening(self) -> None:
+        print("Notify stopped.")
+        self.bot.stop_polling()
+
 
 if __name__ == "__main__":
     bot = NotifyBot(
         bot_token=os.getenv("BOT_API_TOKEN"),
-        spotify=Spotify(
+        spotify=SpotifyHandler(
             client_id=os.getenv("SPOTIFY_CLIENT_ID"),
             client_secret=os.getenv("SPOTIFY_CLIENT_SECRET"),
             redirect_uri=os.getenv("REDIRECT_URI"),
@@ -173,6 +236,5 @@ if __name__ == "__main__":
 
     try:
         bot.start_listening()
-
     except Exception as e:
         print(f"Error starting bot: {e}")
--- server.py
+++ server.py
@@ -1,74 +1,98 @@
 import os
-import sqlite3
-from typing import List
+from typing import *
 
 import requests
 from dotenv import load_dotenv
 from flask import Flask, render_template, request
+from spotipy import Spotify
+
+from main import Database, NotifyBot, SpotifyHandler
 
 load_dotenv()
 
-database: str = os.getenv("NOTIFY_DB")
-
+database: Database = Database(os.getenv("NOTIFY_DB"))
+spotify: Spotify = SpotifyHandler(
+    client_id=os.getenv("SPOTIFY_CLIENT_ID"),
+    client_secret=os.getenv("SPOTIFY_CLIENT_SECRET"),
+    redirect_uri=os.getenv("REDIRECT_URI"),
+    scope="user-read-private user-read-recently-played user-top-read playlist-read-private playlist-read-collaborative",
+)
+bot: NotifyBot = NotifyBot(
+    bot_token=os.getenv("BOT_API_TOKEN"),
+    spotify=SpotifyHandler(
+        client_id=os.getenv("SPOTIFY_CLIENT_ID"),
+        client_secret=os.getenv("SPOTIFY_CLIENT_SECRET"),
+        redirect_uri=os.getenv("REDIRECT_URI"),
+        scope="user-read-private user-read-recently-played user-top-read playlist-read-private playlist-read-collaborative",
+    ),
+    database=Database(os.getenv("NOTIFY_DB")),
+)
 app: Flask = Flask(__name__)
 
 
 @app.route("/callback")
-def callback():
+def callback() -> Any:
     try:
-        conn: sqlite3.Connection = sqlite3.connect(database)
+        # handle authorization denied
+        error: str = request.args.get("error")
+        if error:
+            return render_template("denied.html")
+
+        # handle authorization code
+        code: str = request.args.get("code")
+        if code:
+            # exchange authorization code for an access token
+            token_endpoint: str = "https://accounts.spotify.com/api/token"
+            client_id: str = os.getenv("SPOTIFY_CLIENT_ID")
+            client_secret: str = os.getenv("SPOTIFY_CLIENT_SECRET")
+            redirect_uri: str = os.getenv("REDIRECT_URI")
+            params: List[str] = {
+                "grant_type": "authorization_code",
+                "code": code,
+                "redirect_uri": redirect_uri,
+                "client_id": client_id,
+                "client_secret": client_secret,
+            }
+            response: str = requests.post(token_endpoint, data=params)
+            response_data: str = response.json()
+
+            telegram_user_id: str = request.args.get("state")
+
+            refresh_token: str = response_data.get("refresh_token")
+            access_token: str = response_data.get("access_token")
+
+            spotify_sp: Spotify = spotify.get_user_sp(access_token)
+            spotify_user_display: str = spotify_sp.current_user()["display_name"]
+            spotify_user_id: str = spotify_sp.current_user()["id"]
+
+            if database.user_exists(telegram_user_id):
+                print(
+                    f"User {telegram_user_id} is already stored in {database.database}"
+                )
+
+            else:
+                # store the access token in the database
+                def update_table() -> None:
+                    database.cursor.execute(
+                        "INSERT INTO users (id, telegram_user_id, spotify_user_display, spotify_user_id, refresh_token, access_token) VALUES (?, ?, ?, ?, ?, ?)",
+                        (
+                            None,
+                            telegram_user_id,
+                            spotify_user_display,
+                            spotify_user_id,
+                            refresh_token,
+                            access_token,
+                        ),
+                    )
+
+                database.process(update_table)
+
+            return render_template("auth.html")
 
     except Exception as e:
-        print(f"Error connecting to database: {e}")
+        print(f"An error occurred: {e}")
 
-    cursor: sqlite3.Cursor = conn.cursor()
-
-    # handle authorization denied
-    error: str = request.args.get("error")
-    if error:
-        return render_template("denied.html")
-
-    # handle authorization code
-    code: str = request.args.get("code")
-    if code:
-        # exchange authorization code for an access token
-        token_endpoint: str = "https://accounts.spotify.com/api/token"
-        client_id: str = os.getenv("SPOTIFY_CLIENT_ID")
-        client_secret: str = os.getenv("SPOTIFY_CLIENT_SECRET")
-        redirect_uri: str = os.getenv("REDIRECT_URI")
-        params: List[str] = {
-            "grant_type": "authorization_code",
-            "code": code,
-            "redirect_uri": redirect_uri,
-            "client_id": client_id,
-            "client_secret": client_secret,
-        }
-        response: str = requests.post(token_endpoint, data=params)
-        response_data: str = response.json()
-
-        # get user_id
-        user_id: str = request.args.get("state")
-
-        refresh_token: str = response_data.get("refresh_token")
-        access_token: str = response_data.get("access_token")
-
-        # store the access token in the database
-        try:
-            cursor.execute(
-                "UPDATE users SET refresh_token = ?, access_token = ? WHERE user_id = ?",
-                (refresh_token, access_token, user_id),
-            )
-            conn.commit()
-        except sqlite3.Error as e:
-            print("Error storing access token:", e)
-            conn.rollback()
-
-        return render_template("auth.html")
-
-    cursor.close()
-    conn.close()
-
-    # handle other errors
+    # handle any errors
     return render_template("error.html")
 
 
